54c883a82ac496b1fbbd01f2adc1e8ff585ee7fd
raffrasson <raf.frasson@gmail.com>
2022-03-21 21:10:24 -0300

    configura jwt

---
+++ helpers/jwtGen.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+ 
+const jwtConfig = {
+  expiresIn: '1d',
+};
+
+const SECRET = process.env.JWT_SECRET || 'senha'; 
+
+module.exports = (payload = {}) => jwt.sign({ data: payload }, SECRET, jwtConfig);
