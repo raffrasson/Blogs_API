3b698790c3af12b9ec2458ef2b6ade1360a4ee8f
raffrasson <raf.frasson@gmail.com>
2022-03-24 13:38:45 -0300

    completa requisito 6

--- routers/categoriesRouter.js
+++ routers/categoriesRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { create } = require('../controllers/categoriesController');  
+const { create, getAll } = require('../controllers/categoriesController');  
 const { categoriesValidation, auth } = require('../middlewares');
 
 const categoriesRouter = express.Router();
 
 categoriesRouter.post('/categories', auth, categoriesValidation, create);
-// categoriesRouter.get('/categories', auth, categoriesController.getAll);
+categoriesRouter.get('/categories', auth, getAll);
 // categoriesRouter.get('/categories/:id', auth, categoriesController.getById);
 
 module.exports = categoriesRouter;
